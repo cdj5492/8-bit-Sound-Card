9688294a75dc04fb13f581048a98c9199679db55
IanDolfi <dolfiIan@gmail.com>
2022-03-05 14:25:18 -0500

    created project

---
+++ Test1.vhd
@@ -0,0 +1,41 @@
+-- Copyright (C) 2018  Intel Corporation. All rights reserved.
+-- Your use of Intel Corporation's design tools, logic functions 
+-- and other software and tools, and its AMPP partner logic 
+-- functions, and any output files from any of the foregoing 
+-- (including device programming or simulation files), and any 
+-- associated documentation or information are expressly subject 
+-- to the terms and conditions of the Intel Program License 
+-- Subscription Agreement, the Intel Quartus Prime License Agreement,
+-- the Intel FPGA IP License Agreement, or other applicable license
+-- agreement, including, without limitation, that your use is for
+-- the sole purpose of programming logic devices manufactured by
+-- Intel and sold by Intel or its authorized distributors.  Please
+-- refer to the applicable agreement for further details.
+
+-- PROGRAM		"Quartus Prime"
+-- VERSION		"Version 18.1.0 Build 625 09/12/2018 SJ Lite Edition"
+-- CREATED		"Sat Mar 05 14:14:14 2022"
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all; 
+
+LIBRARY work;
+
+ENTITY Test1 IS 
+	PORT
+	(
+		A :  IN  STD_LOGIC;
+		S :  OUT  STD_LOGIC
+	);
+END Test1;
+
+ARCHITECTURE bdf_type OF Test1 IS 
+
+
+
+BEGIN 
+S <= A;
+
+
+
+END bdf_type;
